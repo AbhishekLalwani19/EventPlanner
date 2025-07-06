fa486bccdd92aa612efd1db8aab1897fc318216c
AbhishekLalwani <abhisheklalwani2005@gmail.com>
2025-07-06 08:50:12 +0530

    update

--- Server/index.js
+++ Server/index.js
@@ -2,11 +2,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 // import mongoose from 'mongoose';    
 import connectDB from './src/config/db.js';
 import AuthRouter from './src/routes/authRouter.js';
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/auth", AuthRouter);
